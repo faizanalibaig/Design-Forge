9e0474a3053666057def7bcb61f517dcecc084ae
faizanalibaig <riazfiaz6181@gmail.com>
2025-01-27 21:14:02 +0500

    bootstrap

---
+++ bootstrap/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="bg-primary">
+            <h1 className="text-primary">Hello, Bootstrap!</h1>
+            <p className="text-danger fw-bold">
+                This is a bold, red paragraph using Bootstrap classes.
+            </p>
+        </div>
+    );
+}
+
+export default App;
